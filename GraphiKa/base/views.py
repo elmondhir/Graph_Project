876a22390ce8a1eb30e4a100701f0e10c6e59286
CHAALAL Mohamed Elmondhir <94637607+elmondhir@users.noreply.github.com>
2023-12-29 18:44:27 +0100

    updated with pk

--- GraphiKa/base/views.py
+++ GraphiKa/base/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "base/home.html")
     # return HttpResponse("Home page")
 
 
-def room(request):
-    return render(request, "room.html")
+def room(request, pk):
+    return render(request, "base/room.html")
     # return HttpResponse("Room page")
